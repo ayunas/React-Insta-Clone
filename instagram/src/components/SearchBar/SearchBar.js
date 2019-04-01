49262de60a07584235491d0d2c77f2c5ddf7b46c
ayunas <ayunas@gmail.com>
2019-04-01 16:54:50 -0500

    added instagram icons to the <SearchBar />

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import person from './person.jpg';
+import heart from './heart.png';
+import logo from './instagram-logo.png';
+import text from './instagram-text-logo.png';
+import diamond from './circle-diamond.png';
 
 class SearchBar extends React.Component {
 
     render() {
 
         return (
-            <header> Instagram </header>
+            
+            <header> 
+                <img src={logo}/>
+                <img src={text}/>
+                <div>search</div>
+                <img src={diamond}/>
+                <img src={heart}/>
+                <img src={person}/>
+            </header>
         )
     }
 }
